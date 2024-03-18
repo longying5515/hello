761bd2a335b865444b40042d1a53e4e1a0ca99d0
LongYing <longying5515@gmail.com>
2024-03-18 15:49:30 +0800

    thread

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
+// 继承 Thread 类创建线程
+class MyThread extends Thread {
+    public void run() {
+        // 线程的执行逻辑
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread running: " + i);
+            try {
+                Thread.sleep(1000); // 暂停1秒
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("main start...");
-        Thread t = new Thread() {
-            public void run() {
-                System.out.println("thread run...");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {}
-                System.out.println("thread end.");
-            }
-        };
-        t.start();
-        try {
-            Thread.sleep(20);
-        } catch (InterruptedException e) {}
-        System.out.println("main end...");
+        // 创建线程对象
+        MyThread thread = new MyThread();
+
+        // 启动线程
+        thread.start();
     }
 }
